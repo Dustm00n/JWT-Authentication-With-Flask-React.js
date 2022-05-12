c37f4b97c0aa0dca8ab7b4eef2ff30142e9e2edd
Dustm00n <gerardoserranosoto@gmail.com>
2022-05-12 21:55:13 +0000

    login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { actions } = useContext(Context);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/demo">
+                Features
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav two">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">
+                Signup
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to="/login"
+                onClick={() => actions.logOut(history)}
+              >
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,87 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      url: "https://3001-dustm00n-jwtauthenticat-vc6dwlrzbbt.ws-us45.gitpod.io/?vscodeBrowserReqId=1652388259038",
+      signup: null,
+      logged: null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // // Use getActions to call a function within a fuction
+      // exampleFunction: () => {
+      // 	getActions().changeColor(0, "green");
+      // },
 
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      // getMessage: () => {
+      // 	// fetching data from the backend
+      // 	fetch(process.env.BACKEND_URL + "/api/hello")
+      // 		.then(resp => resp.json())
+      // 		.then(data => setStore({ message: data.message }))
+      // 		.catch(error => console.log("Error loading message from backend", error));
+      // },
+      // changeColor: (index, color) => {
+      // 	//get the store
+      // 	const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+      // 	//we have to loop the entire demo array to look for the respective index
+      // 	//and change its color
+      // 	const demo = store.demo.map((elm, i) => {
+      // 		if (i === index) elm.background = color;
+      // 		return elm;
+      // 	});
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+      // 	//reset the global store
+      // 	setStore({ demo: demo });
+
+      // },
+      signUp: (formData, history) => {
+        const { url } = getStore();
+        fetch(`${url}/api/signup`, {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("FLUX DATA", data);
+            sessionStorage.setItem("token", JSON.stringify(data));
+            setStore({ signup: data });
+            history.push("/home");
+          })
+          .catch((error) => console.log("HA OCURRIDO UN ERROR", error));
+      },
+      login: (formData, history) => {
+        const { url } = getStore();
+        fetch(`${url}/api/login`, {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("FLUX DATA", data);
+            sessionStorage.setItem("token", JSON.stringify(data));
+            history.push("/home");
+            setStore({ logged: data });
+          })
+          .catch((error) => console.log("HA OCURRIDO UN ERROR", error));
+      },
+      logOut: (history) => {
+        sessionStorage.removeItem("token");
+        history.push("/login");
+      },
+    },
+  };
 };
 
 export default getState;
